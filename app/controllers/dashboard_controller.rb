79ce19bad6e3cd3f630c0cc9b23d70a141d14c9c
Matt Rothstein <it@4beauty.com>
2017-03-17 13:01:53 -0400

    display time off requests from calendar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def index
+    @time_off_requests = TimeOffRequest.all
   end
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,23 @@
 module DashboardHelper
+  def alert_color(status)
+    case status
+    when "approved"
+      "success"
+    when "declined"
+      "danger"
+    when "pending"
+      "warning"
+    end
+  end
+  def glyphicon(status)
+    case status
+    when "approved"
+      "glyphicon-ok-sign"
+    when "declined"
+      "glyphicon-remove-sign"
+    when "pending"
+      "glyphicon-question-sign"
+    end
+  end
+
 end
