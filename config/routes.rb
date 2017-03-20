35b99f2b55bc0e9c2f654ba330949144dbb036a5
Matt Rothstein <it@4beauty.com>
2017-03-20 16:43:51 -0400

    use prefix for devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,14 @@ Rails.application.routes.draw do
   root to: "dashboard#index"
   get 'dashboard/index'
   resources :user_types
-  get "users" => 'users#index', as: 'users'
-  get 'users/:id' => 'users#show', as: 'user'
-  get "users/:id/edit" => 'users#edit', as: 'edit_user'
-  patch "users/:id" => 'users#update'
-  delete "users/:id" => 'users#destroy'
+  resources :users
   resources :purchase_order_requests
   resources :project_approval_requests
   resources :time_off_requests
-    devise_for :users, controllers: {
+    devise_for :users, :path_prefix => 'my', controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
     invitations: 'users/invitations'
   }
+  resources :users
 end
