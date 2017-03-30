58d2db61c088967d6b460d8d33d6e8c3120e6738
Matt Rothstein <it@4beauty.com>
2017-03-30 12:56:46 -0400

    aws paperclip configuration error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,6 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_host_name => "#{ENV['AWS_REGION']}.s3.amazonaws.com",
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
@@ -105,7 +104,7 @@ Rails.application.configure do
     },
     :s3_region => ENV.fetch('AWS_REGION')
   }
-
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
